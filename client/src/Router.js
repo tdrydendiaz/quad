85be21b9e783bcd968107ef16064fac2765dc77c
Ubuntu <tania@jenVM.kv3ek21wnhleridwtpu5dw5ehe.cwx.internal.cloudapp.net>
2019-07-11 13:07:56 +0000

    change made

--- client/src/Router.js
+++ client/src/Router.js
@@ -19,7 +19,7 @@ function RoutingExample() {
             <Link to="/Unit">How to override from the Child/Parent</Link>
           </button>
         
-<p>HelloWorld</p>
+<p>Tania understands docker things</p>
 
         <Route exact path="/" />
         <Route path="/Unit" component={Unit} />
